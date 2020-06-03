0349287bde3685ed3b9a0d7513e7ceb6c96b3afa
Ádám Hegedűs <adam.hegedus@covalensolutions.com>
2020-06-03 19:26:56 +0200

    Routing

--- crwn-clothing/src/pages/homepage/homepage.component.jsx
+++ crwn-clothing/src/pages/homepage/homepage.component.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
-
+import { Switch, Route } from 'react-router-dom';
 import Directory from '../../components/directory/directory.component'
 
 import './homepage.styles.scss';
 
-const HomePage = () => (
-  <div className='homepage'>
-    <h1>Welcome to my Homepage</h1>
-    <Directory></Directory>
+const OptionA = (props) => (
+  <div>
+    <h1>Option A</h1>
   </div>
 );
+const OptionB = (props) => (
+  <div>
+    <h1>Option B</h1>
+  </div>
+);
+const OptionC = (props) => (
+  <div>
+    <h1>Option C</h1>
+  </div>
+);
+
+function HomePage({ match }) {
+    return (
+      <div className='homepage'>
+        <Switch>
+          <Route path={`${match.path}/active`} component={OptionA} />
+          <Route path={`${match.path}/closed`} component={OptionB} />
+          <Route path={`${match.path}/reports`} component={OptionC} />
+        </Switch>
+        <h1>Welcome to my Homepage</h1>
+        <Directory />
+      </div>
+  );
+}
 
 export default HomePage;
