11ab7cbac0599a395e06e501eb72d9716919f444
Ádám Hegedűs <adam.hegedus@covalensolutions.com>
2020-06-07 14:43:58 +0200

    Implement sign-in and sign in with Google using Firebase

--- crwn-clothing/src/App.jsx
+++ crwn-clothing/src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./components/header/header.component";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/stop.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 import "./App.css";
 
@@ -16,6 +17,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route path="/signIn" component={SignInAndSignUp} />
         </Switch>
       </div>
     );
---
+++ crwn-clothing/src/firebase/firebase.utils.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyBRBietMd3a5Hkn6tBnnzNm211Qq_XLdnE",
+    authDomain: "crown-db-a0579.firebaseapp.com",
+    databaseURL: "https://crown-db-a0579.firebaseio.com",
+    projectId: "crown-db-a0579",
+    storageBucket: "crown-db-a0579.appspot.com",
+    messagingSenderId: "339270607394",
+    appId: "1:339270607394:web:2a34a41f947060426ea07b",
+    measurementId: "G-4ZJ37VSVL0"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: 'select_account'
+});
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
