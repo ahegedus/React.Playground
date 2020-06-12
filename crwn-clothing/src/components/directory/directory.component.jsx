255b36af8287c6b2428aca62d399ff509e461c5f
Hegedűs Ádám Imre <hegedus.adam.imre@hotmail.com>
2020-06-12 12:28:56 +0200

    Directory reducer introduced and used

--- crwn-clothing/src/components/directory/directory.component.jsx
+++ crwn-clothing/src/components/directory/directory.component.jsx
@@ -1,32 +1,22 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectDirectorySections } from "../../redux/directory/directory.selectors";
 
 import MenuItem from "../menu-item/menu-item.component";
-import SECTIONS_DATA from "./sections.data.js";
 
 import "./directory.styles.scss";
 
-class Directory extends React.Component {
-  constructor(props) {
-    super(props);
+const Directory = ({ sections }) => (
+  <div className="directory-menu">
+    {sections.map(({ id, ...rest }) => (
+      <MenuItem key={id} {...rest} />
+    ))}
+  </div>
+);
 
-    this.state = {
-      sections: SECTIONS_DATA,
-    };
-  }
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections,
+});
 
-  render() {
-    return (
-      <div className="directory-menu">
-        {/* {this.state.sections.map(({title, imageUrl, id, size, linkUrl}) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl} />
-                ))} */}
-
-        {this.state.sections.map(({ id, ...rest }) => (
-          <MenuItem key={id} {...rest} />
-        ))}
-      </div>
-    );
-  }
-}
-
-export default Directory;
+export default connect(mapStateToProps)(Directory);
