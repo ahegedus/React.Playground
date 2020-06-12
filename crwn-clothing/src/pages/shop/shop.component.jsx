76102d4e5d81ce911f30e3eb3887d42f70a7111c
Hegedűs Ádám Imre <hegedus.adam.imre@hotmail.com>
2020-06-12 12:40:39 +0200

    Shop collection via redux

--- crwn-clothing/src/pages/shop/shop.component.jsx
+++ crwn-clothing/src/pages/shop/shop.component.jsx
@@ -1,30 +1,22 @@
 import React from "react";
-import SHOP_DATA from "./shop.data";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-import "../../components/preview-collection/preview-collection.component";
+import { selectShopCollections } from "../../redux/shop/shop.selectors";
 
 import "./shop.styles.scss";
 import PreviewCollection from "../../components/preview-collection/preview-collection.component";
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    {collections.map(({ id, ...rest }) => (
+      <PreviewCollection key={id} {...rest} />
+    ))}
+  </div>
+);
 
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
+const mapStateToProps = createStructuredSelector({
+  collections: selectShopCollections,
+});
 
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...rest }) => (
-          <PreviewCollection key={id} {...rest} />
-        ))}
-      </div>
-    );
-  }
-}
-
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
