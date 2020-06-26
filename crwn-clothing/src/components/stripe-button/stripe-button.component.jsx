4bcbd7855b017cfda3301328fb1edfe8a6e22247
Hegedűs Ádám Imre <hegedus.adam.imre@hotmail.com>
2020-06-26 08:27:05 +0200

    Stripe integration done

---
+++ crwn-clothing/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+import "./stripe-button.styles.scss";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51GyAWYLZG9syVz8o9cfzG7YshNM4DFvPL3qAe04TDDhTo5ArgPW83x06booUheYHsDR0iupqJN2PEUbfDkq0Xt4600WwpWbLkx";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      panelLabel="Pay Now"
+      name="CRWN Clothing"
+      billingAddress
+      shippingAddress
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
